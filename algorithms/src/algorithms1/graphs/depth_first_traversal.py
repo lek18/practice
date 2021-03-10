bfcef6652b7d3ddd8c0a1cc47750fbe24c7c4c5d
Luis Ek <52711401+lek18@users.noreply.github.com>
2021-03-09 19:41:43 -0600

    new changes

--- algorithms/src/algorithms1/graphs/depth_first_traversal.py
+++ algorithms/src/algorithms1/graphs/depth_first_traversal.py
@@ -2,16 +2,47 @@ class graph:
     def __init__(self,gdict = None):
         if gdict is None:
             gdict = {}
-        self.gdict = {}
+        self.gdict = gdict
+
+    def addVertex(self,node1,node2):
+        if node1 not in self.gdict:
+            self.gdict[node1] = {node2}
+        else:
+            self.gdict[node1].add(node2)
 
     # check for the visited and unvisited nodes - dfs
 
 def dfs(graph,start,visited = None):
+    # steps = []
     if visited is None:
         visited = set()
     visited.add(start)
+    print(start)
 
-    for next in graph[start] - visited:
-        dfs(graph,next,visited)
+    if start in graph:
+        for next in graph[start] - visited:
+            dfs(graph,next,visited)
 
     return visited
+
+
+## disconnected graph
+disc_vertices = [[0,2],[2,1],[1,0],[1,2],[0,3]]
+
+#create graph
+myDisconnectedGraph = graph()
+for node1,node2 in disc_vertices:
+    myDisconnectedGraph.addVertex(node1,node2)
+
+
+myDisconnectedGraph.gdict
+
+myDisconnectedGraph.addVertex(node1,node2)
+
+dfs(start=0,graph=myDisconnectedGraph.gdict)
+
+### a not disconnected graph
+
+gdict = { 0 : set([1,2)}
+
+dfs(gdict,0)
